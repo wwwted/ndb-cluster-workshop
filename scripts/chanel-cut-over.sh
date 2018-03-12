185840ede48b29f4a635e47aa20259ef5090813e
wwwted <ted.wennmark@gmail.com>
2018-03-12 15:39:46 +0100

    added start-replication.sh

---
+++ scripts/chanel-cut-over.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+#   MySQL Site-1               MySQL Site-2
+# ==========================================
+#   MySQL-53316 -------------> MySQL-53326 (slave)
+#   MySQL-53317                MySQL-53327
+#   to
+#   MySQL-53316                MySQL-53326
+#   MySQL-53317 -------------> MySQL-53327 (slave)
+#
+# Demo script to move replication chanel as shown above
+#
+
+echo "Starting channel cut-over from (MySQL-53316 ---X---> MySQL-53326) to (MySQL-53317 -----> MySQL-53327 (slave))"
+echo "press <ENTER> to continue"
+read
+
+# Break/Stop replication between MySQL-53316 ---X---> MySQL-53326
+echo "Breaking replication between 53316 -> 53326"
+mysql -uroot -h127.0.0.1 -P53326 -e "stop slave"
+mysql -uroot -h127.0.0.1 -P53326 -e "show slave status\G"
+echo "press <ENTER> to continue"
+read
+
+# Configure replication MySQL-53317 -----> MySQL-53327 (slave)
+
+epoch=`mysql -uroot -h127.0.0.1 -P53327 -se"SELECT MAX(epoch) FROM mysql.ndb_apply_status\G" |grep epoch| cut -f2 -d:|sed  "s/ //g"`
+[ -z $epoch ] && echo "epoch empty, exiting.." && exit 1
+
+LOGFILE=`mysql -uroot -h127.0.0.1 -P53317 -se"SELECT SUBSTRING_INDEX(next_file, '/', -1), next_position FROM mysql.ndb_binlog_index WHERE epoch >= $epoch ORDER BY epoch ASC LIMIT 1\G" |grep next_file|cut -f2 -d:|sed  "s/ //g"`
+LOGPOS=`mysql -uroot -h127.0.0.1 -P53317 -se"SELECT SUBSTRING_INDEX(next_file, '/', -1), next_position FROM mysql.ndb_binlog_index WHERE epoch >= $epoch ORDER BY epoch ASC LIMIT 1\G" |grep next_position|cut -f2 -d:|sed  "s/ //g"`
+
+echo "Epoch from 53317 = ($epoch), LOGFILE and LOGPOS fetched from 53317; LOGFILE=($LOGFILE) LOGPOS=($LOGPOS)"
+echo "press <ENTER> to continue"
+read
+
+[ -z $LOGFILE ] && echo "LOGFILE empty, exiting.." && exit 1
+[ -z $LOGPOS ] && echo "LOGPOS empty, exiting.." && exit 1
+
+mysql -uroot -h127.0.0.1 -P53327 << EOL
+
+change master to
+	master_host='127.0.0.1',
+	master_port=53317,
+	master_user='rep',
+	master_password='rep',
+	master_log_file="$LOGFILE",
+	master_log_pos=$LOGPOS;
+EOL
+
+# start replication
+mysql -uroot -h127.0.0.1 -P53327 -e "start slave"
+mysql -uroot -h127.0.0.1 -P53327 -e "show slave status\G"
+
---
+++ scripts/start-replication.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# 1) Start 2 Cluster using MCM template mcm-templates/replication-cluster
+#    mcm < mcm-templates/replication-cluster
+# 2) Run this script
+#
+# MySQL Cluster-1              MySQL Cluster-2
+#   MySQL-53316   ----------->   MySQL-53326 (slave)
+#
+
+#
+# Configure replication MySQL-53316 -> MySQL-53326 (slave)
+#
+echo "Configure replication: MySQL-53316 -> MySQL-53326 (slave)"
+LOGFILE=`mysql -uroot -h127.0.0.1 -P53316 -e "show master status\G"|grep File|cut -f2 -d:|sed  "s/ //g"`
+LOGPOS=`mysql -uroot -h127.0.0.1 -P53316 -e "show master status\G"|grep Position|cut -f2 -d:|sed  "s/ //g"`
+echo "LOGFILE=$LOGFILE,LOGPOS=$LOGPOS"
+
+mysql -uroot -h127.0.0.1 -P53326 << EOL
+change master to
+	master_host='127.0.0.1',
+	master_port=53316,
+	master_user='rep',
+	master_password='rep',
+	master_log_file="$LOGFILE",
+	master_log_pos=$LOGPOS;
+EOL
+
+# Add grants needed for replication on masters (Cluster-1)
+mysql -uroot -h127.0.0.1 -P53316 << EOL
+SET sql_log_bin=0;
+create user rep@'%' identified by 'rep';
+grant replication slave on *.* to rep@'%';
+SET sql_log_bin=1;
+EOL
+
+mysql -uroot -h127.0.0.1 -P53317 << EOL
+SET sql_log_bin=0;
+create user rep@'%' identified by 'rep';
+grant replication slave on *.* to rep@'%';
+SET sql_log_bin=1;
+EOL
+
+mysql -uroot -h127.0.0.1 -P53326 -e "show slave status\G"
+echo "press <ENTER> to start replication"
+read
+# start replication
+#
+mysql -uroot -h127.0.0.1 -P53326 -e "start slave"
+mysql -uroot -h127.0.0.1 -P53326 -e "show slave status\G"
+
