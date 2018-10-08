b7f1a847b630e49857057a6e56d9f70879290ac2
wwwted <ted.wennmark@gmail.com>
2018-10-08 11:04:59 +0200

    counter stats added

---
+++ scripts/counter-stats.sql
@@ -0,0 +1,22 @@
+-- watch "mysql -uroot -S /tmp/mysql.mycluster.50.sock < counter-stats.sql"
+-- mysql -uroot -S /tmp/mysql.mycluster.50.sock < counter-stats.sql
+
+create database IF NOT EXISTS test;
+use test;
+drop table IF EXISTS c1, c2;
+create table c1 (select * from ndbinfo.counters);
+select sleep(1);
+create table c2 (select * from ndbinfo.counters);
+select c2.node_id,
+       c2.block_name as bl_name,
+       c2.block_instance as bl_inst,
+       c2.counter_name as block_name,
+       c2.val - c1.val as value
+from test.c1, test.c2
+where 
+  c1.node_id = c2.node_id
+  and c1.block_name = c2.block_name
+  and c1.block_instance = c2.block_instance
+  and c1.counter_id = c2.counter_id
+  and (c2.val-c1.val) != 0;
+
