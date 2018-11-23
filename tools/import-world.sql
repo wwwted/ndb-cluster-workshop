8ebfef576d3d1d5e67e0d8034cf5529098b35a40
wwwted <ted.wennmark@gmail.com>
2018-11-23 10:56:24 +0100

    added FK to world tables

--- tools/import-world.sql
+++ tools/import-world.sql
@@ -11,3 +11,5 @@ alter table countrylanguage engine=ndbcluster;
 alter table city engine=ndbcluster;
 alter table country engine=ndbcluster;
 
+ALTER TABLE city ADD CONSTRAINT `city_ibfk_1` FOREIGN KEY (`CountryCode`) REFERENCES `country` (`Code`);
+ALTER TABLE countrylanguage ADD CONSTRAINT `countryLanguage_ibfk_1` FOREIGN KEY (`CountryCode`) REFERENCES `country` (`Code`);
