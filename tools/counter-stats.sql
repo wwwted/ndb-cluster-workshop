43e583a252de7681fc3c2057bb43efc216398fc2
wwwted <ted.wennmark@gmail.com>
2019-06-18 07:32:01 +0200

    updated paths

--- tools/counter-stats.sql
+++ tools/counter-stats.sql
@@ -1,5 +1,4 @@
--- watch "mysql -uroot -S /tmp/mysql.mycluster.50.sock < counter-stats.sql"
--- mysql -uroot -S /tmp/mysql.mycluster.50.sock < counter-stats.sql
+-- watch "mysql -uroot -proot -P3310 -h127.0.0.1 < tools/counter-stats.sql"
 
 create database IF NOT EXISTS stats;
 use stats;
@@ -12,11 +11,11 @@ select c2.node_id,
        c2.block_instance as bl_inst,
        c2.counter_name as block_name,
        c2.val - c1.val as value
-from test.c1, test.c2
+from stats.c1, stats.c2
 where 
   c1.node_id = c2.node_id
   and c1.block_name = c2.block_name
   and c1.block_instance = c2.block_instance
   and c1.counter_id = c2.counter_id
   and (c2.val-c1.val) != 0;
-
+drop database stats;
