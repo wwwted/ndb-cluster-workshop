04ab24bcb83d6436429ecda3657d73f3b498c4ff
wwwted <ted.wennmark@gmail.com>
2020-05-14 15:08:59 +0200

    updated for latest 7.6

--- scripts/createEnv.sh
+++ scripts/createEnv.sh
@@ -5,11 +5,11 @@
 # Download NDB 7.6.9 and MCM 1.4.7 + change BIN_DIR to folder with binaries in tar.gz format.
 # TARGET_DIR is where you downloaded the workshop (git clone https://github.com/wwwted/ndb-cluster-workshop.git)
 
-TARGET_DIR="/home/ted/ndb-cluster-workshop"
-NDB_BIN="/home/ted/src/mysql-cluster-gpl-7.6.9-linux-glibc2.12-x86_64.tar.gz"
-MCM_BIN="/home/ted/src/mcm-1.4.7-linux-glibc2.12-x86-64bit.tar.gz"
+TARGET_DIR="/home/opc/workshop"
+NDB_BIN="/home/opc/mysql-cluster-gpl-7.6.14-linux-glibc2.12-x86_64.tar.gz"
+MCM_BIN="/home/opc/mcm-1.4.8-linux-glibc2.12-x86-64bit.tar.gz"
 
-# You should not have to change anything below 
+# You should not have to change anything below (only if you change binary versions)
 cd $TARGET_DIR
 
 if [ ! -f "$NDB_BIN" ]
@@ -18,7 +18,7 @@ then
    exit 1
 fi
 tar xzf $NDB_BIN
-mv mysql-cluster-gpl-7.6.9-linux-glibc2.12-x86_64 cluster-769
+mv mysql-cluster-gpl-7.6.14-linux-glibc2.12-x86_64 cluster-7614
 
 if [ ! -f "$MCM_BIN" ]
 then
@@ -26,11 +26,11 @@ then
    exit 1
 fi
 tar xzf $MCM_BIN
-mv mcm-1.4.7-linux-linux-glibc2.12-x86-64bit/mcm1.4.7 .
-rmdir mcm-1.4.7-linux-linux-glibc2.12-x86-64bit
+mv mcm-1.4.8-linux-glibc2.12-x86-64bit/mcm1.4.8 .
+rmdir mcm-1.4.8-linux-glibc2.12-x86-64bit
 
-ln -s mcm1.4.7/ mcm_bin
-ln -s cluster-769 ndb_bin
+ln -s mcm1.4.8/ mcm_bin
+ln -s cluster-7614 ndb_bin
 
 cp mcm_bin/etc/mcmd.ini .
 echo "manager-directory = $TARGET_DIR/mcm_data" >> $TARGET_DIR/mcmd.ini
@@ -40,3 +40,4 @@ chmod 660 $TARGET_DIR/mcmd.ini
 
 echo "export WS_HOME=$TARGET_DIR" > $TARGET_DIR/setenv
 echo "export PATH=$TARGET_DIR/ndb_bin/bin:$TARGET_DIR/mcm_bin/bin:$PATH" >> $TARGET_DIR/setenv
+
